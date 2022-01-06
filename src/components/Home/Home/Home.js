f32079c532dedc59915b102e050223fb31b145a5
sayamkhanraj <snsayamkhanraj@gmail.com>
2022-01-06 19:14:02 +0600

    fontawesomeAdded

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import Support from '../Support/Support';
 
 
 const Home = () => {
          return (
                   <div>
                       <Header />
-                      <Banner />                  
+                      <Banner />
+                      <Support />
+                      <Footer />                  
                   </div>
          );
 };
