4ff3e81160264f0c4e3a006a026b3eb5ab9efd35
sayamkhanraj <snsayamkhanraj@gmail.com>
2022-01-06 20:50:14 +0600

    blogsAdded

--- src/components/Home/Blogs/Blogs.js
+++ src/components/Home/Blogs/Blogs.js
@@ -3,17 +3,29 @@ import './Blogs.css'
 
 const Blogs = () => {
          return (
-                  <div className="container">
-                           <h2 className="blog-titel text-center">OUR BLOG</h2>
+                  <div className="container my-5">
+                           <h2 className="blog-titel text-center my-5">OUR BLOG</h2>
                            <div className="d-flex justify-content-center align-items-center row">
-                           <div className="col-md-4">
-                                    <img src="" alt="" />
+                           <div className="col-md-4 blog-img ">
+                                    <div className="blog-img"><img src="https://i.ibb.co/1bDvpmP/blog-1.jpg" alt="blog-img" /></div>
+                                    <div className="text-center blog-content ">
+                                             <h3>A guide to latest trends</h3>
+                                             <span className="text-muted">By Admin</span>
+                                    </div>
                            </div>
                            <div className="col-md-4">
-
+                                    <div className="blog-img"><img src="https://i.ibb.co/hH60sLY/blog-2.jpg" alt="blog-img" /></div>
+                                    <div className="text-center blog-content">
+                                             <h3>A guide to latest trends</h3>
+                                             <span className="text-muted">By Admin</span>
+                                    </div>
                            </div>
                            <div className="col-md-4">
-
+                                    <div className="blog-img"><img src="https://i.ibb.co/HTwLK75/blog-3.jpg" alt="blog-img" /></div>
+                                    <div className="text-center blog-content">
+                                             <h3>A guide to latest trends</h3>
+                                             <span className="text-muted">By Admin</span>
+                                    </div>
                            </div>
                            </div>
                   </div>
