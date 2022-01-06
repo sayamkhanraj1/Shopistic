8be274487606c476b45c86a8f61cf7a3fe45c289
sayamkhanraj <snsayamkhanraj@gmail.com>
2022-01-06 21:36:33 +0600

    editBlogs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home/Home/Home';
+import Blogs from './components/Home/Blogs/Blogs';
 
 
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="blogs" element={<Blogs />} />
         
       </Routes>
     </div>
