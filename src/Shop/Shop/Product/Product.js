39663bc5e212ecc5a500452b9782ea4cd267e7a7
ariyanislam7576 <ma5665711@gmail.com>
2022-01-06 22:31:09 -0800

    shop added

---
+++ src/Shop/Shop/Product/Product.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Card, Col } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+const Product = (props) => {
+    const { image, price, title, id } = props.p
+    return (
+
+        <Col>
+            <Card>
+                <Card.Img  style={{width: '150px'}} variant="top" src={image} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {price}
+                    </Card.Text>
+                    <NavLink to={`/moreproducts/${id}`}>
+                    <button className="btn-primary">Bay Now</button>
+                    </NavLink>
+                    
+                </Card.Body>
+            </Card>
+        </Col>
+
+    );
+};
+
+export default Product;
---
+++ src/Shop/Shop/SingleProduct/SingleProduct.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+
+const SingleProduct = () => {
+    const { id } = useParams()
+    const [shipping, setShipping] = useState([])
+    useEffect(() => {
+        fetch(`https://fakestoreapi.com/products/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setShipping(data)
+            })
+    }, [])
+    const {image, title, price} = data
+    return (
+        <Row xs={1} md={2} className="g-4">
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src={image} />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+        </Row>
+    );
+};
+
+export default SingleProduct;
