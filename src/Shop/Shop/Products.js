84fefc82b616bea52d6d4d1dee08613fab0e001e
ariyanislam7576 <ma5665711@gmail.com>
2022-01-06 22:20:51 -0800

    commit

---
+++ src/Shop/Shop/Products.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { Container, Row } from 'react-bootstrap';
+import Product from './Product/Product';
+
+const Products = () => {
+    const [product, setProduct] = useState([])
+    useEffect(()=>{
+        fetch('https://fakestoreapi.com/products')
+        .then(res => res.json())
+        .then(data => setProduct(data))
+    },[])
+    return (
+        <Container>
+            <Row xs={1} md={2} className="g-4">
+                {
+                    product.map(p => <Product key={p.id}
+                    p={p}
+                    ></Product>)
+                }
+            </Row>
+        </Container>
+    );
+};
+
+export default Products;
